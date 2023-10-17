f8801b136908dfe1e54f45a3434e20064a2528e4
Christian <ChristianArroyo308@yahoo.com>
2023-10-17 01:53:14 -0700

    can now scrape most recent price of Eden Jellyfish on AH

--- index.js
+++ index.js
@@ -27,17 +27,17 @@ async function scrapeTarget() {
     await browser.close();
 };
 
-async function scrapeSite2() {
+async function scrapeEdenJellyFish() {
     const browser = await puppeteer.launch({
     headless: "new", // Set to true for headless mode, false for a visible browser
   });
     const page = await browser.newPage();
-    await page.goto('https://www.albertsons.com/shop/product-details.960320580.html');
+    await page.goto('https://edenxi.com/tools/item/4443?stack=true');
 
     const pageTitle = await page.title();
 
     // xpath
-    const [productNamePath] = await page.$x('//*[@id="pg960320580"]');
+    const [productNamePath] = await page.$x('//*[@id="root"]/div/div[1]/div/div/div/div/div/div/div[2]/div/div[2]/div[1]/div[1]/div/text()');
     const text = await productNamePath.getProperty('textContent')
     const rawText = await text.jsonValue()
     
@@ -46,13 +46,15 @@ async function scrapeSite2() {
     // const priceContent = await pricePath.innerHTML()
     // const rawPrice = await priceContent.jsonValue()
 
+    const tbodyElement = await page.$('table tbody tr:nth-child(1) td:nth-child(4)');
+    const cellText = await page.evaluate(tableData => tableData.textContent, tbodyElement);
 
-    console.log('Page Title:', pageTitle);
-    console.log('Text:', rawText);
-    // console.log('Price:', pricePath);
+    console.log('Eden Page Title:', pageTitle);
+    console.log('Item Text:', rawText);
+    console.log('Price:', cellText);
 
     await browser.close();
 };
 
 // scrapeTarget()
-scrapeSite2()
+scrapeEdenJellyFish()
