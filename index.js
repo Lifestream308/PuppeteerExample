134419f9d75d8e7a92ae01841874554d0b1d0872
Christian <ChristianArroyo308@yahoo.com>
2023-11-08 02:21:43 -0800

    added test to check title of craigslist or any website name entered into function

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const scrapeTarget = require('./targetTitle');
 const scrapeEdenJellyFish = require('./edenJelly');
+const testSite = require('./testSite')
 
 // scrapeTarget()
-scrapeEdenJellyFish()
+// scrapeEdenJellyFish()
+// testSite('https://www.amazon.com')
+testSite('https://losangeles.craigslist.org/search/sss#search=1~gallery~0~0')
---
+++ testSite.js
@@ -0,0 +1,17 @@
+const puppeteer = require('puppeteer');
+
+async function testSite(website) {
+    const browser = await puppeteer.launch({
+    headless: "new", // Set to true for headless mode, false for a visible browser
+  });
+    const page = await browser.newPage();
+    await page.goto(website);
+
+    const pageTitle = await page.title();
+
+    console.log('Page Title:', pageTitle);
+
+    await browser.close();
+};
+
+module.exports = testSite
