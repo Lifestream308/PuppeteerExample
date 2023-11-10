f5bf5d43a5286ca628e56e5d009f0b931c16fbee
Christian <ChristianArroyo308@yahoo.com>
2023-11-10 02:59:24 -0800

    edenJelly wasn't scraping correctly. Fixed as of now

--- edenJelly.js
+++ edenJelly.js
@@ -11,20 +11,34 @@ async function scrapeEdenJellyFish() {
 
     // xpath
     const [productNamePath] = await page.$x('//*[@id="root"]/div/div[1]/div/div/div/div/div/div/div[2]/div/div[2]/div[1]/div[1]/div/text()');
-    const text = await productNamePath.getProperty('textContent')
-    const rawText = await text.jsonValue()
+    const text = await productNamePath?.getProperty('textContent')
+    const rawText = await text?.jsonValue()
+    // this worked as of 11/10/2023 but it sometimes doesn't work? Just note that it seems to not always work
     
     // const [pricePath] = await page.$x('');
     // const priceContent = await pricePath.getProperty('textContent')
     // const priceContent = await pricePath.innerHTML()
     // const rawPrice = await priceContent.jsonValue()
 
-    const tbodyElement = await page.$('table tbody tr:nth-child(1) td:nth-child(4)');
-    const cellText = await page.evaluate(tableData => tableData.textContent, tbodyElement);
+    const cellText = await page.$eval('table tbody tr:nth-child(1) td:nth-child(4)', (cell) => {
+      return cell.textContent.trim();
+    });
+    // this worked as of 11/10/2023
+
+    const seller = await page.$eval('table tbody tr:nth-child(1) td:nth-child(2)', (cell) => {
+      return cell.textContent.trim();
+    });
+    // this worked as of 11/10/2023
+
+    const dateSold = await page.$eval('table tbody tr:nth-child(1) td:nth-child(1)', (cell) => {
+      return cell.textContent.trim();
+    });
+    // this worked as of 11/10/2023
 
     console.log('Eden Page Title:', pageTitle);
-    console.log('Item Text:', rawText);
-    console.log('Price:', cellText);
+    // console.log('Item Text:', rawText);
+    // console.log('Price:', cellText);
+    console.log(`Item: ${rawText} Price: ${cellText} Seller: ${seller} Date: ${dateSold}`);
 
     await browser.close();
 };
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const scrapeEdenJellyFish = require('./edenJelly');
 const testSite = require('./testSite')
 
 // scrapeTarget()
-// scrapeEdenJellyFish()
+scrapeEdenJellyFish()
+// testSite('https://edenxi.com')
 // testSite('https://www.amazon.com')
-testSite('https://losangeles.craigslist.org/search/sss#search=1~gallery~0~0')
+// testSite('https://losangeles.craigslist.org/search/sss#search=1~gallery~0~0')
